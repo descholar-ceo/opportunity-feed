38af6bbd92774352f059f8bf963a966a95cbfe91
descholar-ceo <emmanuellamugi@gmail.com>
2021-06-18 00:06:52 +0200

    Add team members for a given post

--- src/components/Opportunity.js
+++ src/components/Opportunity.js
@@ -10,6 +10,7 @@ const Opportunity = ({ opportunity }) => {
     compensation,
     skills,
     organizations,
+    members,
   } = opportunity;
   let currency; let minAmount; let maxAmount; let
     periodicity;
@@ -37,6 +38,16 @@ const Opportunity = ({ opportunity }) => {
           <img src={picture} alt={name} />
         </div>
         <p>{name}</p>
+      </div>
+    );
+  })) : '';
+  const teamMembers = members.length !== 0 ? (members.slice(0, 7).map((member) => {
+    const { name, username, picture } = member;
+    return (
+      <div key={username}>
+        <div>
+          <img src={picture || 'https://res.cloudinary.com/descholar/image/upload/v1619552670/n7cdvpeomukfih68zzcz.svg'} alt={name} title={name} />
+        </div>
       </div>
     );
   })) : '';
@@ -76,8 +87,8 @@ const Opportunity = ({ opportunity }) => {
         {skillSet}
       </div>
       <div className="display-grid td-rightmost">
-        <h2 className="current-chapter-container">{organizationDefined}</h2>
-        <h3>Chapter 17</h3>
+        <p className="current-chapter-container">{organizationDefined}</p>
+        <p className="team-members">{teamMembers}</p>
         <button className="primary-btn" type="button">View</button>
       </div>
     </div>
@@ -91,6 +102,7 @@ Opportunity.propTypes = {
     remote: PropTypes.bool.isRequired,
     locations: PropTypes.string.isRequired,
     organizations: PropTypes.shape().isRequired,
+    members: PropTypes.shape().isRequired,
     skills: PropTypes.shape({
       length: PropTypes.func.isRequired,
       map: PropTypes.func.isRequired,
